6678a5cda701df0ae10097e21e3d4d095b1de0c9
tanukesensei <luanvs.hack@gmail.com>
2022-01-20 23:02:23 -0300

    Finish the project

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ window.onscroll = () => {
     header.classList.remove("active");
   }
 };
+
+/**
+ * Insert down the date of the offer will end.
+ */
+let countDate = new Date("January 20, 2023 00:00:00").getTime();
+
+function CountDown() {
+  let now = new Date().getTime();
+  gap = countDate - now;
+
+  let second = 1000;
+  let minute = second * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
+
+  let d = Math.floor(gap / day);
+  let h = Math.floor((gap % day) / hour);
+  let m = Math.floor((gap % hour) / minute);
+  let s = Math.floor((gap % minute) / second);
+
+  document.getElementById("day").innerText = d;
+  document.getElementById("hour").innerText = h;
+  document.getElementById("minute").innerText = m;
+  document.getElementById("second").innerText = s;
+}
+
+setInterval(function () {
+  CountDown();
+}, 1000);
